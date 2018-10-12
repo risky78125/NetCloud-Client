528cb27e0bc263cd64762e96cfdaaf74b70556d3
武奇 <risky5200@gmail.com>
2018-10-12 14:45:51 +0800

    角色管理假数据添加

--- src/app/main/role/role.component.ts
+++ src/app/main/role/role.component.ts
@@ -10,7 +10,43 @@ import {Router} from '@angular/router';
 })
 export class RoleComponent implements OnInit {
 
-  dataSource: any = {};
+  dataSource: RoleBean[] = [
+    {
+      roleId: 1,
+      roleName: '超级管理员',
+      moduleNames: '角色管理、管理员管理、资费管理、账务账号、业务账号、账单、报表'
+    },
+    {
+      roleId: 2,
+      roleName: '管理员',
+      moduleNames: '角色管理、资费管理、账务账号、业务账号、账单、报表'
+    },
+    {
+      roleId: 3,
+      roleName: '资费管理员',
+      moduleNames: '资费管理'
+    },
+    {
+      roleId: 4,
+      roleName: '账务账号管理员',
+      moduleNames: '管理员管理'
+    },
+    {
+      roleId: 5,
+      roleName: '业务账号管理员',
+      moduleNames: '角色管理、资费管理'
+    },
+    {
+      roleId: 6,
+      roleName: '账单管理员',
+      moduleNames: '角色管理、管理员管理'
+    },
+    {
+      roleId: 7,
+      roleName: '报表管理员',
+      moduleNames: '管理员管理、资费管理'
+    }
+  ];
 
   totalCount = 8;
 
@@ -29,4 +65,11 @@ export class RoleComponent implements OnInit {
     console.log(pageIndex);
   }
 
+}
+
+class RoleBean {
+  constructor(
+    public roleId: number,
+    public roleName: string,
+    public moduleNames: string) {}
 }
